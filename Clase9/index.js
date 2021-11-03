f6df99860e60aad33799bf49978bd67135cb71e1
Diego Ezequiel Estela <die.estela@gmail.com>
2021-11-03 16:48:10 -0300

    Confiurando Pug

--- Clase9/index.js
+++ Clase9/index.js
@@ -3,8 +3,8 @@ const app = express();
 const fs = require("fs");
 const port = 4040;
 
-app.set("views", "./views");
 app.set("view engine", "pug");
+app.set("views", "./views");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
