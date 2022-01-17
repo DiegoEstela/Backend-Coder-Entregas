0e37b16c52fcc371ea826843c37450b437067532
Diego Ezequiel Estela <die.estela@gmail.com>
2022-01-17 01:46:32 -0300

    Entrega process

--- Clase22/dbknex.js
+++ Clase22/dbknex.js
@@ -1,3 +1,7 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const knex = require("knex")({
   client: "mysql",
   connection: {
@@ -24,4 +28,30 @@ knex.schema
     console.log(err);
   });
 
-module.exports = knex;
+const mongoose = require("mongoose");
+const { Schema, model } = require("mongoose");
+
+mongoose.connect(process.env.MONGO_DB);
+
+mongoose.connection.on("open", () => {
+  console.log("Base de datos conectada con exito");
+});
+
+mongoose.connection.on("error", () => {
+  console.log("Error al conectarse a la base de datos");
+});
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const userModel = model("user", userSchema);
+
+module.exports = { knex, userModel };
--- Clase22/index.js
+++ Clase22/index.js
@@ -2,10 +2,19 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 const knex = require("./dbknex");
-
+const dotenv = require("dotenv");
+const parseArg = require("minimist");
+dotenv.config();
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const random = require("./routes/randoms");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
 const products = require("./products");
 const Mensajes = require("./sms");
 const sms = new Mensajes("sms.txt");
+const mongoStore = require("connect-mongo");
+const { userModel } = require("./dbknex");
 
 const faker = require("faker");
 const { normalize, schema } = require("normalizr");
@@ -15,6 +24,17 @@ const prod = new products();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// process
+
+dotenv.config();
+
+const options = {
+  default: { port: 8080 },
+  alias: { p: "port" },
+};
+
+let arguments = parseArg(process.argv.slice(2), options);
 
 // server
 const http = require("http");
@@ -29,6 +49,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + "/public"));
 
+app.use("/api/randoms", random);
+
 // Data mock con Faker
 
 let productosFaker = [];
@@ -40,6 +62,94 @@ for (let i = 0; i < 5; i++) {
     descripcion: faker.commerce.productAdjective(),
     foto: faker.image.imageUrl(),
   });
+}
+
+const authorize = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next();
+    return;
+  }
+  res.redirect("/login");
+};
+
+// passport
+
+app.use(
+  session({
+    store: mongoStore.create({
+      mongoUrl: process.env.MONGO_DB,
+    }),
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: true,
+    resave: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  "local-login",
+  new LocalStrategy((username, password, done) => {
+    userModel.findOne({ username: username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        console.log(`User not found`);
+        return done(null, false);
+      }
+      if (!isValidPassword(user, password)) {
+        console.log(`Invalid password`);
+        return done(null, false);
+      }
+      return done(null, user);
+    });
+  })
+);
+
+passport.use(
+  "local-signup",
+  new LocalStrategy(
+    {
+      passReqToCallback: true,
+    },
+    (req, username, password, done) => {
+      userModel.findOne({ username: username }, (err, user) => {
+        if (err) {
+          console.log(`Error in signup ${err}`);
+        }
+        if (user) {
+          console.log(`User already exists`);
+          return done(null, false);
+        }
+        const newUser = {
+          username: username,
+          password: bcrypt.hashSync(password, 10),
+        };
+
+        userModel.create(newUser, (err, user) => {
+          if (err) {
+            console.log(`Error in saving user: ${err}`);
+            return done(err);
+          }
+          return done(null, user);
+        });
+      });
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  userModel.findById(id, done);
+});
+
+function isValidPassword(user, password) {
+  return bcrypt.compareSync(password, user.password);
 }
 
 // schema de sms
@@ -69,17 +179,35 @@ app.get("/productos-test", (req, res) => {
 // Ruta login
 
 app.get("/login", (req, res) => {
-  res.sendFile(__dirname + "/public/login.html");
+  res.sendFile(`${__dirname}/public/login.html`);
 });
 
-app.post("/login", (req, res) => {
-  const user = req.body;
-  if (user.username == "Diego") {
-    console.log(user.username);
-    req.session.user = user.username;
-    res.redirect("/");
-    return;
-  }
+app.get("/signup", (req, res) => {
+  res.sendFile(`${__dirname}/public/signup.html`);
+});
+
+app.get("/getUsers", (req, res) => {
+  res.send(user);
+});
+
+app.post(
+  "/signup",
+  passport.authenticate("local-signup", {
+    successRedirect: "/login",
+    failureRedirect: "/signup",
+  })
+);
+
+app.post(
+  "/login",
+  passport.authenticate("local-login", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
+
+app.get("/logout", (req, res) => {
+  req.logOut();
   res.redirect("/login");
 });
 io.on("connection", async (socket) => {
@@ -96,6 +224,19 @@ io.on("connection", async (socket) => {
   });
 });
 
-server.listen(port, () => {
-  console.log("Server running on port: " + port);
+app.get("/info", (req, res) => {
+  let data = {
+    "argumentos de entrada": process.argv.slice(2),
+    "sistema opertativo": process.platform,
+    "version de node": process.version,
+    rss: process.memoryUsage().rss,
+    path: process.execPath,
+    processId: process.pid,
+    "carpeta proyecto": process.cwd(),
+  };
+  res.json({ data });
+});
+
+server.listen(arguments.port, () => {
+  console.log(`Server run on port ${arguments.port}`);
 });
